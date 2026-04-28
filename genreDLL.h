4a6fa750b75d704c5970e5b542f046f025e575fc
chrism18719-cmyk <chrism18719@gmail.com>
2026-04-28 14:23:44 -0400

    started working on the genreDLL.h

---
+++ genreDLL.h
@@ -0,0 +1,16 @@
+#ifndef genreDLL_H
+#define genreDLL_H
+
+#include <iostream>
+#include <string>
+#include <moviesSLL.h> 
+
+using namespace std; 
+
+class GenreNode 
+{
+    public:
+
+};
+
+#endif // !genreDLL_H
